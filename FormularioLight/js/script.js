076ba9317a5bb0ae1bbd36c5976a5aba200298c9
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2019-10-31 23:05:39 -0300

    Update
    Alteração do webconfig do fluxo de trabalho.
    Adição do projeto formulario-light

---
+++ FormularioLight/js/script.js
@@ -0,0 +1,42 @@
+var allOk = true;
+
+$(document).ready(function()
+{
+    btnSalvar_Click();
+    slcUnidadeTrabalho_Changed();
+});
+
+function slcUnidadeTrabalho_Changed()
+{
+    $("#slcUnidadeTrabalho").change(function()
+    {
+        if($(this).val() == "000")
+        {
+            $("#iptNomeCidade").val("");
+            $("#iptNomeCidade").css("display","block").addClass("iptObg");
+        }else{
+            $("#iptNomeCidade").hide().removeClass("iptObg");
+        }
+    });
+}
+
+function btnSalvar_Click()
+{
+    $("#btnSalvar").click(function()
+    {
+        var iptObg = $(".iptObg");
+
+        for(let I=0; I<iptObg.length; I++)
+        {
+            if(iptObg.eq(I).val() == "")
+            {
+                allOk = false;
+                iptObg.eq(I).css({"border-color":"#FF0000"});
+
+                setTimeout(function(){
+                    iptObg.eq(I).css({"border-color":"#CCCCCC"});
+                },3000);
+            }
+        }
+    });
+}
